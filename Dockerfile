6bc271ca4bfb119c8f8b311c07a16d15a5008273
riverfog7 <cjw9770@gmail.com>
2025-10-23 20:44:16 +0900

    working nginx with docker

--- Dockerfile
+++ Dockerfile
@@ -12,14 +12,16 @@ COPY .python-version .python-version
 RUN uv sync --locked --no-editable --no-cache
 
 FROM python:3.13-alpine3.22
-RUN apk add --no-cache nginx
+RUN apk add --no-cache nginx envsubst bash
 COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
 COPY --from=deps /app/.venv /app/.venv
 
-COPY /app /app
+COPY . /app
 WORKDIR /app
 
 ENV PORT=8000
+ENV IMAGE_PORT=25
+ENV VLLM_PORT=26
 ENV PERSISTENT_VOLUME_DIR=/workspace
 ENV DOWNLOAD_MODELS=0
 
--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 DOWNLOAD_MODELS=${DOWNLOAD_MODELS:-0}
 PORT=${PORT:-8888}
 PERSISTENT_VOLUME_DIR=${PERSISTENT_VOLUME_DIR:-/workspace}
-PERSISTENT_VOLUME_DIR=$(cd "${PERSISTENT_VOLUME_DIR}"; pwd)
 
 if [ ! -d "${PERSISTENT_VOLUME_DIR}" ]; then
   printf "Configured persistent volume directory ${PERSISTENT_VOLUME_DIR} does not exist. Exiting.\n" >&2
   exit 1
 fi
+PERSISTENT_VOLUME_DIR=$(cd "${PERSISTENT_VOLUME_DIR}"; pwd)
 cd "${PERSISTENT_VOLUME_DIR}"
 
 # check if PERSISTENT_VOLUME_DIR/models exists
@@ -30,3 +30,12 @@ else
     printf "Model directory %s exists. Skipping model download.\n" "${MODEL_DIR}"
   fi
 fi
+
+NGINX_TEMPLATE="${SCRIPT_DIR}/nginx.conf.template"
+NGINX_CONF="/etc/nginx/nginx.conf"
+
+printf "Generating nginx config from template: %s -> %s\n" "$NGINX_TEMPLATE" "$NGINX_CONF"
+envsubst '\$PORT \$VLLM_PORT \$IMAGE_PORT' < "$NGINX_TEMPLATE" > "$NGINX_CONF"
+
+printf "Starting Nginx in foreground...\n"
+nginx -g 'daemon off;'
